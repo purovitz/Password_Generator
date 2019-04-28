5e0013ce5c6c112995080f52973bd8d81cbd59d4
purovitz <purovskyy@gmail.com>
2019-04-28 18:55:50 +0200

    „Creation”

---
+++ src/main/java/DisplayMenu.java
@@ -0,0 +1,16 @@
+public class DisplayMenu {
+    public static void display() {
+        System.out.println("=====Password Generator=====");
+        System.out.println("1 - Password Length:");
+        System.out.println("2 - ");
+        System.out.println("3 - ");
+        System.out.println("4 - ");
+        System.out.println("5 - ");
+        System.out.println("6 - ");
+        System.out.println("7 - ");
+        System.out.println("8 - ");
+        System.out.println("9 - ");
+        System.out.println("10 - ");
+        System.out.println("0 - Wylacz aplikacje");
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+
+
+        boolean isAplicationRun = true;
+        while (isAplicationRun) {
+            DisplayMenu.display(); {
+
+            }
+        }
+    }
+}
+
