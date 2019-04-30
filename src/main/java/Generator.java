bffc51aed5f728860895cc8760b56e397d57c99b
purovitz <purovskyy@gmail.com>
2019-04-30 21:52:21 +0200

    Attempts to combine several functions

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -7,30 +7,70 @@ public class Generator {
     public static final String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static final String numbers = "0123456789";
     public static final String symbols = "!@#$%&*()_+-=[]|,./?><";
+    static int passwordLengths;
 
     public static Integer passwordLength() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the password length:");
+        System.out.println("1 - Enter the password length:");
         System.out.print(":::");
-        boolean isPasswordLengthCorrect = true;
-        while (!isPasswordLengthCorrect) {
-            try {
-                Integer passwordAnswer = scanner.nextInt();
-                if (passwordAnswer > 32) {
-                    System.out.println("Password can be up to 32 digits long, try again :)");
-                    System.out.println("");
-                    System.out.println("Enter the password length:");
-                    System.out.print(":::");
-                    isPasswordLengthCorrect = false;
-                } else {
-                    isPasswordLengthCorrect = true;
-                    return passwordAnswer;
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("You gave the number in the wrong format");
-                isPasswordLengthCorrect = false;
+
+        try {
+            while ((passwordLengths = scanner.nextInt()) > 32) {
+                System.out.println("Password can be up to 32 digits long, try again :).");
+                System.out.println(":::");
             }
+
+        } catch (InputMismatchException e) {
+            System.out.println("You gave the number in the wrong format.");
         }
-        return null;
+        return passwordLengths;
+    }
+
+    public static void passwordTest(){
+        try {
+            passwordLength();
+        } catch (InputMismatchException e){
+            passwordLength();
+        }
+    }
+
+
+
+    public static boolean includeSymbols() {
+        Scanner scannerr = new Scanner(System.in);
+        System.out.println("2 - Include symbols(Yes/No):");
+        System.out.print(":::");
+        String answer = scannerr.next().toLowerCase();
+        if (answer.charAt(0) == 'y') {
+            System.out.println("");
+            System.out.println("You want your password to contain symbols.");
+            return true;
+        } else {
+            System.out.println("");
+            System.out.println("You don't want your password to contain symbols");
+            return false;
+        }
     }
 }
+
+
+/*
+public static Integer passwordLength() {
+        Scanner scanner = new Scanner(System.in);
+        boolean validInput = true;
+        System.out.println("1 - Enter the password length:");
+        System.out.print(":::");
+
+        try {
+            while ((passwordLength = scanner.nextInt()) > 32) {
+                System.out.println("Password can be up to 32 digits long, try again :).");
+                System.out.println(":::");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("You gave the number in the wrong format.");
+        }
+        return passwordLength;
+    }
+ */
+
+
